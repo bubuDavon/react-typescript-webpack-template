5e3de1d86f0963158ab98ccbb0807a1dc81e1613
bubuDavon <guoweijie@corp.netease.com>
2025-02-27 11:48:12 +0800

    feat: use latest eslint

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+import typescriptParser from '@typescript-eslint/parser';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
+import hooksPlugin from 'eslint-plugin-react-hooks';
+
+const ignorePaths = ['.vscode', 'node_modules', 'webpack'];
+export default [
+  // js code
+  js.configs.recommended,
+  // code style
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: { ...globals.browser },
+    },
+  },
+  // tsx code
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: { modules: true },
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': hooksPlugin,
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      ...reactPlugin.configs['jsx-runtime'].rules,
+      ...hooksPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+    },
+  },
+  // custom rules
+  {
+    rules: {
+      'react-hooks/exhaustive-deps': 0,
+    },
+  },
+  // ignore paths
+  {
+    ignores: ignorePaths,
+  },
+];
---
+++ src/Child2.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Child2 = () => {
+  console.log('Child rendered')
+
+  return (
+    <div>
+      <h3>Child Component2, no props</h3>
+    </div>
+  )
+}
