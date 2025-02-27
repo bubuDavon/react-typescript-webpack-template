4d82996918cf2adda85d387f1e6239f839789a3c
bubuDavon <guoweijie@corp.netease.com>
2025-02-27 11:48:12 +0800

    feat: use latest eslint

--- src/Parent.tsx
+++ src/Parent.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Child } from './Child'
-
+import { Child2 } from './Child2'
 export const Parent = () => {
   const [count, setCount] = useState(0)
   const [staticValue, setStaticValue] = useState('static');
@@ -12,6 +12,7 @@ export const Parent = () => {
       <button onClick={() => setCount(count + 1)}>Increment Count</button>
       <p>Count: {count}</p>
       <Child value={staticValue} />
+      <Child2 />
     </div>
   )
 }
